ebeabe56213931e93b5c7d5d2a1ee33cf37354e4
unsuigo <unsuigo@gmail.com>
2024-06-18 15:51:25 +0200

    new models

--- Assets/_CompleteGame/Scripts/Cutter.cs
+++ Assets/_CompleteGame/Scripts/Cutter.cs
@@ -5,11 +5,6 @@ namespace GardenCutter
 {
     public class Cutter : MonoBehaviour
     {
-        //The number of vertices to create per frame
-        private const int NUM_VERTICES = 12;
-
-        [SerializeField] [Tooltip("The blade object")]
-        private GameObject _blade = null;
 
         [SerializeField] [Tooltip("The empty game object located at the tip of the blade")]
         private GameObject _tip = null;
@@ -17,14 +12,6 @@ namespace GardenCutter
         [SerializeField] [Tooltip("The empty game object located at the base of the blade")]
         private GameObject _base = null;
 
-        [SerializeField] [Tooltip("The mesh object with the mesh filter and mesh renderer")]
-        private GameObject _meshParent = null;
-
-        [SerializeField] [Tooltip("The number of frame that the trail should be rendered for")]
-        private int _trailFrameLength = 3;
-
-        [SerializeField] [ColorUsage(true, true)] [Tooltip("The colour of the blade and trail")]
-        private Color _colour = Color.red;
 
         [SerializeField] [Tooltip("The amount of force applied to each side of a slice")]
         private float _forceAppliedToCut = 3f;
@@ -39,74 +26,7 @@ namespace GardenCutter
         private Vector3 _triggerEnterBasePosition;
         private Vector3 _triggerExitTipPosition;
 
-        // void Start()
-        // {
-        //     _meshParent.transform.position = Vector3.zero;
-        //     _meshCutPlain = new Mesh();
-        //     _meshParent.GetComponent<MeshFilter>().mesh = _meshCutPlain;
-        //
-        //     Material trailMaterial = Instantiate(_meshParent.GetComponent<MeshRenderer>().sharedMaterial);
-        //     trailMaterial.SetColor("grin", _colour);
-        //     _meshParent.GetComponent<MeshRenderer>().sharedMaterial = trailMaterial;
-        //
-        //     Material bladeMaterial = Instantiate(_blade.GetComponent<MeshRenderer>().sharedMaterial);
-        //     bladeMaterial.SetColor("blue", _colour);
-        //     _blade.GetComponent<MeshRenderer>().sharedMaterial = bladeMaterial;
-        //
-        //     _vertices = new Vector3[_trailFrameLength * NUM_VERTICES];
-        //     _triangles = new int[_vertices.Length];
-        //
-        //     _previousTipPosition = _tip.transform.position;
-        //     _previousBasePosition = _base.transform.position;
-        // }
-        //
-        // void LateUpdate()
-        // {
-        //     //Reset the frame count one we reach the frame length
-        //     if (_frameCount == (_trailFrameLength * NUM_VERTICES))
-        //     {
-        //         _frameCount = 0;
-        //     }
-        //
-        //     //Draw first triangle vertices for back and front
-        //     _vertices[_frameCount] = _base.transform.position;
-        //     _vertices[_frameCount + 1] = _tip.transform.position;
-        //     _vertices[_frameCount + 2] = _previousTipPosition;
-        //     _vertices[_frameCount + 3] = _base.transform.position;
-        //     _vertices[_frameCount + 4] = _previousTipPosition;
-        //     _vertices[_frameCount + 5] = _tip.transform.position;
-        //
-        //     //Draw fill in triangle vertices
-        //     _vertices[_frameCount + 6] = _previousTipPosition;
-        //     _vertices[_frameCount + 7] = _base.transform.position;
-        //     _vertices[_frameCount + 8] = _previousBasePosition;
-        //     _vertices[_frameCount + 9] = _previousTipPosition;
-        //     _vertices[_frameCount + 10] = _previousBasePosition;
-        //     _vertices[_frameCount + 11] = _base.transform.position;
-        //
-        //     //Set triangles
-        //     _triangles[_frameCount] = _frameCount;
-        //     _triangles[_frameCount + 1] = _frameCount + 1;
-        //     _triangles[_frameCount + 2] = _frameCount + 2;
-        //     _triangles[_frameCount + 3] = _frameCount + 3;
-        //     _triangles[_frameCount + 4] = _frameCount + 4;
-        //     _triangles[_frameCount + 5] = _frameCount + 5;
-        //     _triangles[_frameCount + 6] = _frameCount + 6;
-        //     _triangles[_frameCount + 7] = _frameCount + 7;
-        //     _triangles[_frameCount + 8] = _frameCount + 8;
-        //     _triangles[_frameCount + 9] = _frameCount + 9;
-        //     _triangles[_frameCount + 10] = _frameCount + 10;
-        //     _triangles[_frameCount + 11] = _frameCount + 11;
-        //
-        //     _meshCutPlain.vertices = _vertices;
-        //     _meshCutPlain.triangles = _triangles;
-        //
-        //     //Track the previous base and tip positions for the next frame
-        //     _previousTipPosition = _tip.transform.position;
-        //     _previousBasePosition = _base.transform.position;
-        //     _frameCount += NUM_VERTICES;
-        // }
-
+     
         private void OnTriggerEnter(Collider other)
         {
             _triggerEnterTipPosition = _tip.transform.position;
@@ -156,7 +76,7 @@ namespace GardenCutter
 
             Rigidbody rigidbody = slices[1].GetComponent<Rigidbody>();
             Vector3 newNormal = transformedNormal + Vector3.up * _forceAppliedToCut;
-            rigidbody.AddForce(newNormal, ForceMode.Impulse);
+            // rigidbody.AddForce(newNormal, ForceMode.Impulse);
         }
     }
 }
